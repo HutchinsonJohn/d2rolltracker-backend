46344ddc350a0ea1ef22be7a36660cc765d63616
HutchinsonJohn <80031202+HutchinsonJohn@users.noreply.github.com>
2024-03-18 21:45:23 -0400

    feat: manifest will backup to s3

--- src/utils/manifestHelpers.ts
+++ src/utils/manifestHelpers.ts
@@ -7,6 +7,12 @@ import {
 } from 'bungie-api-ts/destiny2'
 import logger from './logger.js'
 import manifest from '../manifest.js'
+import {
+  GetObjectCommand,
+  ListObjectsCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3'
 
 export type DestinyManifestSlices = DestinyManifestSlice<
   [
@@ -45,6 +51,9 @@ export default async function loadManifest(retry = 0) {
       logger.info('Retrying')
       return loadManifest(retry + 1)
     }
+    if (!manifest.path) {
+      loadManifestFromS3()
+    }
     return
   }
   const destinyManifest = manifestResponse.Response
@@ -71,5 +80,80 @@ export default async function loadManifest(retry = 0) {
   })
   manifest.path = path
   manifest.definitions = manifestDefs
+
   logger.info('Loaded manifest')
+
+  if (!process.env.BUCKET_NAME || !process.env.BUCKET_REGION) {
+    return
+  }
+  await backupManifestToS3(manifestDefs)
+}
+
+async function backupManifestToS3(manifestDefs: DestinyManifestSlices) {
+  const BUCKET_NAME = process.env.BUCKET_NAME
+  const BUCKET_REGION = process.env.BUCKET_REGION
+  if (!BUCKET_NAME || !BUCKET_REGION) {
+    logger.error('No bucket name or region for S3')
+    return
+  }
+  const s3Client = new S3Client({ region: BUCKET_REGION })
+  const command = new ListObjectsCommand({ Bucket: BUCKET_NAME })
+  s3Client.send(command, async (err, data) => {
+    if (err) {
+      logger.error(err)
+      return
+    }
+    if (
+      (data?.Contents?.[0]?.LastModified ?? new Date(0)) <
+      new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
+    ) {
+      logger.info('Uploading manifest to S3')
+      await uploadManifestToS3(manifestDefs)
+    }
+  })
+}
+
+function loadManifestFromS3() {
+  const BUCKET_NAME = process.env.BUCKET_NAME
+  const BUCKET_REGION = process.env.BUCKET_REGION
+  if (!BUCKET_NAME || !BUCKET_REGION) {
+    logger.error('No bucket name or region for S3')
+    return
+  }
+  const s3Client = new S3Client({ region: BUCKET_REGION })
+  const command = new GetObjectCommand({ Bucket: BUCKET_NAME, Key: 'manifest' })
+  s3Client.send(command, async (err, data) => {
+    if (err) {
+      logger.error(err)
+      return
+    }
+    if (!data?.Body) {
+      logger.error('No body in manifest object from S3')
+      return
+    }
+    manifest.definitions = JSON.parse(data.Body.toString())
+    logger.info('Loaded manifest from S3')
+  })
+}
+
+async function uploadManifestToS3(manifest: DestinyManifestSlices) {
+  const BUCKET_NAME = process.env.BUCKET_NAME
+  const BUCKET_REGION = process.env.BUCKET_REGION
+  if (!BUCKET_NAME || !BUCKET_REGION) {
+    logger.error('No bucket name or region for S3')
+    return
+  }
+  const s3Client = new S3Client({ region: BUCKET_REGION })
+  const command = new PutObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: 'manifest',
+    Body: JSON.stringify(manifest),
+  })
+  await s3Client.send(command, (err) => {
+    if (err) {
+      logger.error(err)
+      return
+    }
+  })
+  logger.info('Uploaded manifest to S3')
 }
